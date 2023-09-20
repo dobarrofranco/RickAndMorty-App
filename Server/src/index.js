d0b36f94e6b07a4e7d85a24dfade8c4e233cd679
dobarrofranco <mr.francodobarro@gmail.com>
2023-09-20 14:46:41 -0300

    PostgreSQL 01

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 const server = require('./app')
+const { conn } = require('./DB_connection');
 const PORT = 3001;
 
 server.listen(PORT, () => {
+    conn.sync({force: true});
     console.log('Estamos en el puerto: ' + PORT);
 })
